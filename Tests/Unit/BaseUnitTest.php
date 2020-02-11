9546c679a15f9d941a855013a19527ba14e4e3ed
Maksim Lifintcev <hullabaloooooooo@gmail.com>
2020-02-11 17:29:36 +0300

    Initial commit

---
+++ Tests/Unit/BaseUnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class BaseUnitTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function checkTestsAvailable()
+    {
+        $this->assertTrue(true);
+    }
+} 

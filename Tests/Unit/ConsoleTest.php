36ba55172cbd5599954fa5036399cbe7f893f595
Maksim Lifintcev <hullabaloooooooo@DESKTOP-8HJK1VO.localdomain>
2020-03-03 12:40:58 +0300

    ATOM-13 Finish console

--- Tests/Unit/ConsoleTest.php
+++ Tests/Unit/ConsoleTest.php
@@ -2,10 +2,14 @@
 
 namespace Tests\Unit;
 
+use Phox\Nebula\Atom\Implementation\Basics\Collection;
+use Phox\Nebula\Atom\Implementation\Console;
 use Phox\Nebula\Atom\Implementation\Exceptions\ConsoleException;
 use Phox\Nebula\Atom\TestCase;
 use Phox\Nebula\Atom\Notion\Interfaces\IStateContainer;
 use Phox\Nebula\Atom\Implementation\States\ConsoleState;
+use Phox\Nebula\Atom\Notion\Abstracts\Provider;
+use Phox\Nebula\Atom\Notion\Interfaces\ICommand;
 use stdClass;
 
 class ConsoleTest extends TestCase 
@@ -50,5 +54,71 @@ class ConsoleTest extends TestCase
         $this->expectException(ConsoleException::class);
         $this->expectExceptionMessage("Command must match '{module}::{command}' pattern, 'command' was given");
         app()->runConsole(['file', 'command']);
+    }
+
+    /**
+     * @test
+     */
+    public function registerTest()
+    {
+        $mock = $this->getMockBuilder(stdClass::class)->addMethods(['call'])->getMock();
+        $mock->expects($this->once())->method('call');
+        container()->singleton($mock, stdClass::class);
+        $provider = new class extends Provider {
+            public function define(Console $console) {
+                $console->registerCommand(get_class(new class(new stdClass) implements ICommand {
+                    public static string $module = 'atom';
+                    public static string $command = 'unittestcommand';
+
+                    private stdClass $mock;
+                    
+                    public function __construct(stdClass $mock) {
+                        $this->mock = $mock;
+                    }
+
+                    public function run() {
+                        call([$this->mock, 'call']);
+                    }
+                }));
+            }
+        };
+        app()->addProvider($provider);
+        app()->runConsole(['file', 'atom::unittestcommand']);
+    }
+
+    /**
+     * @test
+     */
+    public function optionsTest()
+    {
+        $mock = $this->getMockBuilder(stdClass::class)->getMock();
+        $mock->testCase = $this;
+        container()->singleton($mock, stdClass::class);
+        $provider = new class extends Provider {
+            public function define(Console $console) {
+                $console->registerCommand(get_class(new class(new stdClass) implements ICommand {
+                    public static string $module = 'atom';
+                    public static string $command = 'unittestcommand';
+
+                    private stdClass $mock;
+                    
+                    public function __construct(stdClass $mock) {
+                        $this->mock = $mock;
+                    }
+
+                    public function run() {
+                        $console = get(Console::class);
+                        $this->mock->testCase->assertTrue($console->hasOption('method1') && $console->option('method1') == true && $console->option('method1') === '1');
+                        $this->mock->testCase->assertTrue($console->hasOption('method3'));
+                        $this->mock->testCase->assertFalse($console->hasOption('method2'));
+                        $this->mock->testCase->assertEquals('string', $console->option('q'));
+                        $this->mock->testCase->assertEquals('string with space', $console->option('qwerty'));
+                        $this->mock->testCase->assertInstanceOf(Collection::class, $console->getOptions());
+                    }
+                }));
+            }
+        };
+        app()->addProvider($provider);
+        app()->runConsole(['file', 'atom::unittestcommand', '-q', 'string', '--qwerty', 'string with space', '--method1', '--method3']);
     }
 }
--- src/Implementation/Console.php
+++ src/Implementation/Console.php
@@ -2,7 +2,9 @@
 
 namespace Phox\Nebula\Atom\Implementation;
 
+use Phox\Nebula\Atom\Implementation\Basics\Collection;
 use Phox\Nebula\Atom\Implementation\Exceptions\ConsoleException;
+use Phox\Nebula\Atom\Notion\Interfaces\ICommand;
 
 class Console 
 {
@@ -14,20 +16,94 @@ class Console
 
     protected string $command;
 
-    protected array $options = [];
+    protected Collection $options;
+
+    protected Collection $commands;
 
 	public function __construct(array $arguments)
 	{
+        $this->commands = new Collection(Collection::class);
+        $this->options = new Collection('string');
         $this->arguments = $arguments;
     }
 
+    /**
+	 * Execute command
+	 *
+	 * @return void
+	 */
     public function run()
     {
-        $this->prepare(); 
+        $this->prepare();
+        if (!$this->commands->hasIndex($this->module)) {
+            error(ConsoleException::class, "Module '{$this->module}' does not contain a commands");
+        }
+        $moduleCommands = $this->commands->get($this->module);
+        if (!$moduleCommands->hasIndex($this->command)) {
+            error(ConsoleException::class, "Module '{$this->module}' does not contain '{$this->command}' command");
+        }
+        $command = $moduleCommands->get($this->command);
+        $command->run();
+    }
+
+    /**
+	 * Get value of console option
+	 *
+	 * @param string $name Option name
+	 * @return mixed
+	 */
+
+    public function option(string $name)
+    {
+        switch (true) {
+            case $this->options->hasIndex($name): return $this->options->get($name);
+            case $this->options->hasIndex('-' . $name): return $this->options->get('-' . $name);
+            case $this->options->hasIndex('--' . $name): return $this->options->get('--' . $name);
+            default: return null;
+        }
+    }
+
+    /**
+	 * Check if option exists
+	 *
+	 * @param string $name Option name
+	 * @return boolean
+	 */
+    public function hasOption(string $name) : bool
+    {
+        return !is_null($this->option($name));
+    }
+
+    /**
+     * Get all concole options
+     *
+     * @return Collection
+     */
+    public function getOptions() : Collection
+    {
+        return $this->options;
+    }
+
+    /**
+     * Register command in collection
+     *
+     * @param string $commandClass
+     * @return void
+     */
+    public function registerCommand(string $commandClass)
+    {
+        $module = $commandClass::$module;
+        $command = $commandClass::$command;
+        $this->commands->hasIndex($module) ?: $this->commands->set($module, new Collection(ICommand::class));
+        $moduleCommands = $this->commands->get($module);
+        $moduleCommands->hasIndex($command)
+            ? error(ConsoleException::class, "Command '{$command}' already exists in module '{$module}'")
+            : $moduleCommands->set($command, make($commandClass));
     }
     
     protected function prepare()
     {
+        !empty($this->arguments) ?: error(ConsoleException::class, 'Bad arguments');
         $arguments = $this->arguments;
         $this->caller = $arguments[0];
         unset($arguments[0]);
@@ -41,12 +117,12 @@ class Console
         });
 		foreach ($options as $index => $value) {
 			if (array_key_exists($index + 1, $options)) {
-				$this->options[$value] = true;
+                $this->options->set($value, (string)true);
 				continue;
 			} elseif (array_key_exists($index + 1, $arguments)) {
-				$this->options[$value] = $arguments[$index + 1];
+                $this->options->set($value, $arguments[$index + 1]);
 			} else {
-				$this->options[$value] = true;
+                $this->options->set($value, (string)true);
 			}
 		}
     }
